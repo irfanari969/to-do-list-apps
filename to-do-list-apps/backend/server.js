fe696d21ff96fc4a15e5bb7502c0d1f212762664
irfanari969 <irfanari969@gmail.com>
2026-03-01 15:44:05 +0700

    prepare deploy backend

--- to-do-list-apps/backend/server.js
+++ to-do-list-apps/backend/server.js
@@ -11,7 +11,8 @@ app.use(express.json())
 
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,     
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,     
     user: process.env.DB_USER,    
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME   
